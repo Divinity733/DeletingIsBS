bdd467d4b5792e65bce5f98bc3b829565a405de4
Brennan Litster <brennan@thatoneplace.net>
2015-04-23 13:33:35 -0600

    Saving and Loading Text

--- src/deleting/controller/DeleteDBcontrol.java
+++ src/deleting/controller/DeleteDBcontrol.java
@@ -1,7 +1,17 @@
 package deleting.controller;
 
 import deleting.view.DeleteGuiFrame;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
+
+import javax.swing.JOptionPane;
+
 import deleting.model.QueryInfo;
 
 public class DeleteDBcontrol
@@ -25,7 +35,7 @@ public class DeleteDBcontrol
 	 */
 	public void start()
 	{
-		
+		loadTimeInfo();
 	}
 	
 	/**
@@ -51,5 +61,60 @@ public class DeleteDBcontrol
 	public ArrayList<QueryInfo> getTimingInfoList()
 	{
 		return timingInfoList;
+	}
+	
+	private void loadTimeInfo()
+	{
+		File saveFile = new File("C:/Users/blit1703/Documents/sdfs.txt");
+		try
+		{
+			timingInfoList.clear();
+			Scanner readFileScanner;
+			if (saveFile.exists())
+			{
+				readFileScanner = new Scanner(saveFile);
+				while (readFileScanner.hasNext())
+				{
+					String tempQuery = readFileScanner.nextLine();
+					readFileScanner.next();
+					long tempTime = readFileScanner.nextLong();
+					timingInfoList.add(new QueryInfo(tempQuery, tempTime));
+				}
+				readFileScanner.close();
+			}
+		}
+		catch (IOException current)
+		{
+			this.getDatabase().displayErrors(current);
+		}
+		
+	}
+	
+	public void saveQueryInfo()
+	{
+		String fileName = "C:/Users/blit1703/Documents/sdfs.txt";
+		ArrayList<QueryInfo> output = getTimingInfoList();
+		PrintWriter outputWriter;
+		
+		try
+		{
+			outputWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+			for (QueryInfo current : output)
+			{
+				outputWriter.write(current.getQuery());
+				outputWriter.write(current.getQueryTime() + "\n");
+			}
+			outputWriter.close();
+		}
+		catch (FileNotFoundException noExistingFile)
+		{
+			JOptionPane.showMessageDialog(appFrame, "There is no file there :(");
+			JOptionPane.showMessageDialog(appFrame, noExistingFile.getMessage());
+		}
+		catch (IOException inputOutputError)
+		{
+			JOptionPane.showMessageDialog(appFrame, "There is no file there :(");
+			JOptionPane.showMessageDialog(appFrame, inputOutputError.getMessage());
+		}
 	}
 }
--- src/deleting/view/DeleteGuiFrame.java
+++ src/deleting/view/DeleteGuiFrame.java
@@ -1,18 +1,26 @@
 package deleting.view;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
 import deleting.controller.DeleteDBcontrol;
+import deleting.model.QueryInfo;
 
 public class DeleteGuiFrame extends JFrame
 {
 	private DeleteGuiPanel basePanel;
+	private DeleteDBcontrol baseController;
 	
 	public DeleteGuiFrame(DeleteDBcontrol baseController)
 	{
+		this.baseController = baseController;
 		basePanel = new DeleteGuiPanel(baseController);
 		
 		setupFrame();
+		setupListeners();
 	}
 	
 	private void setupFrame()
@@ -20,5 +28,69 @@ public class DeleteGuiFrame extends JFrame
 		this.setContentPane(basePanel);
 		this.setSize(1000, 1000);
 		this.setVisible(true);
+	}
+	
+	private void setupListeners()
+	{
+		this.addWindowListener(new WindowListener()
+		{
+
+			@Override
+			public void windowActivated(
+					WindowEvent arg0)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowClosed(
+					WindowEvent e)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowClosing(
+					WindowEvent e)
+			{
+				baseController.saveQueryInfo();
+				
+			}
+
+			@Override
+			public void windowDeactivated(
+					WindowEvent e)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowDeiconified(
+					WindowEvent e)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowIconified(
+					WindowEvent e)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowOpened(
+					WindowEvent e)
+			{
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 }
